88d96c28ad8b247cebe3b84622cd7de99cc87f6a
Argyrios Patikos <argyrios@kth.se>
2019-12-11 19:28:41 +0100

    new matlab script for milestone 3

---
+++ M3.m
@@ -0,0 +1,105 @@
+%%%%%%%%% Milestone 3 %%%%%%%%%%%%
+
+%% small signal parameters
+
+%=========NMOS 1,2,5,6=========
+
+%===1,2===%
+%W/L=100u/150n  ID=400u
+g_m2= 6.827E-3;
+r_o2= 4.53E3;
+C_gd2= 29.71E-15;
+
+%W/L=10u/150n  ID=40u
+%g_m2= 684E-6;
+%r_o2= 45.46E3;
+%C_gd2= 2.96E-15;
+
+%W/L=10u/500n  ID=200u
+%g_m2= 1.467E-3;
+%r_o2= 23.96E3;
+%C_gd2= 3.21E-15;
+
+%===6===%
+%W/L=100u/150n  ID=400u
+r_o6= 4.53E3;
+C_gd6= 29.71E-15;
+
+%W/L=10u/500n  ID=200u
+%r_o6= 23.96E3;
+%C_gd6= 3.21E-15;
+
+%===5===%
+%W/L=100u/150n  ID=800u
+r_o5= 2.448E3;
+
+%W/L=10u/500n  ID=400u
+%r_o5= 9.797E3;
+
+%=========PMOS 3,4,7===========
+%===3,4===%
+%W/L=100u/150n  ID=400u
+r_o4= 9.15E3;
+C_gd4= 38.27E-15;
+
+%W/L=  ID=
+%r_o4= 91.24E3/3;
+%C_gd4= ;
+
+%===7===%
+%W/L=100u/150n  ID=400u
+r_o7= 9.15E3;
+g_m7= 4.98E-3;
+C_gd7= 38.27E-15;
+C_gs7= 86.79E-15;
+
+%W/L=10u/150n  ID=40u
+%r_o7= 91.24E3;
+%g_m7= 502.9E-6;
+%C_gd7= 3.817E-15;
+%C_gs7= 8.672E-15;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+R1 = inv(inv(r_o4)+inv((1+ 1.2*g_m2*r_o5)*r_o2));
+
+R2 = inv(inv(r_o7)+inv(r_o6));
+
+C1= C_gd4 + C_gd2 + C_gs7 + C_gd7*g_m7*R2;
+
+C2 = C_gd6 + C_gd7*(1+ 1/(g_m7*R2));
+
+A1= g_m2*R1;
+A2= g_m7*R2;
+Adc= A1*A2;
+
+w_p1= 2*pi*2E6;
+f_p1 = w_p1/(2*pi);
+
+Cc= 1/(g_m7*R1*R2*w_p1);
+
+w_p2= g_m7*Cc/(C1*C2+ C2*Cc+ C1*Cc);                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+f_p2 = w_p2/(2*pi);
+
+%Rz=(C2+Cc)/(Cc*g_m7);
+%Rz= 1/g_m7 + 1/(w_p2*Cc);  %Rz equal to gm_7
+Rz= 2*inv(g_m7);            %Rz greater than g_m7 => zero on LHP
+
+w_z= 1/(Cc*(Rz - 1/g_m7));
+f_z = w_z/(2*pi);
+
+%%%%%%%%%%%%%%%%%%%%%%%%
+syms w_u
+eqn = 1 + (w_u/w_p1)^2 + (w_u/w_p2)^2 + ((w_u)^4)/((w_p1^2)*(w_p2^2)) - (Adc^2)*(1+(w_u/w_z)^2)^2;
+sol = solve(eqn, w_u);
+W_u = double(max(real(vpa(sol))));
+f_u = W_u/(2*pi);
+
+th_1 = atan2(W_u, w_p1)*180/pi;
+
+th_2 = atan2(W_u, w_p2)*180/pi;
+
+th_z = atan2(W_u, w_z)*180/pi;
+
+PM = 180 - th_1 - th_2 + th_z
+
